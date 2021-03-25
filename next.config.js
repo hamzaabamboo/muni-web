7037a12bb9a0656cb2b8f83b4e91a37641d92b0a
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-03-25 15:26:44 +0700

    muni sets enviroment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
-  basePath: ghPages ? "/muni-web" : "",
+  basePath: ghPages ? "/muni-web" : process.env.NEXT_PUBLIC_BASE_URL || "",
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import Head from "next/head";
 import React from "react";
+import {} from "next/image";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import "../styles/globals.css";
 
+const base = process.env.NEXT_PUBLIC_BASE_URL || "";
 function MyApp({ Component, pageProps }) {
+  console.log(process.env);
   return (
     <>
       <Head>
         <title>Create むに App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${base}/favicon.ico`} />
       </Head>
       <ChakraProvider>
         <Component {...pageProps} />
