e84f6f0e5ab5824accd1917929c9dc3d74e36ba1
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-03-25 13:21:52 +0700

    muni tries to cors

--- src/api/getLeaderboardData.ts
+++ src/api/getLeaderboardData.ts
@@ -3,8 +3,12 @@ import { Leaderboard, LeaderboardEntry } from "types/Leaderboard";
 
 export const getLeaderboardData = async (): Promise<Leaderboard> => {
   const res = await Promise.all([
-    axios.get<Leaderboard>("http://www.projectdivar.com/eventdata/t20"),
-    axios.get<Leaderboard>("http://www.projectdivar.com/eventdata/t50"),
+    axios.get<Leaderboard>(
+      "https://hambot.ham-san.net/d4dj/sig?url=http://www.projectdivar.com/eventdata/t20"
+    ),
+    axios.get<Leaderboard>(
+      "https://hambot.ham-san.net/d4dj/sig?url=http://www.projectdivar.com/eventdata/t50"
+    ),
   ]);
   return res
     .map((e) => e.data)
