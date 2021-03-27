e229746dbb8ecd9355e6598d44382965aebb1b74
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-03-27 15:41:55 +0700

    towa fixes proxy stuff

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,8 @@
 export const getProxiedUrl = (url: string) => {
-  return process.env.NODE_ENV === "development"
-    ? "/api/proxy?url=" + url
-    : "https://hambot.ham-san.net/d4dj/sig?url=" + url;
+  if (process.env.NODE_ENV === "development") return "/api/proxy?url=" + url;
+  if (url.match("projectdivar"))
+    return "https://hambot.ham-san.net/d4dj/sig?url=" + url;
+  if (url.match("d4-dj"))
+    return "https://hambot.ham-san.net/d4dj/d4db?url=" + url;
+  return url;
 };
