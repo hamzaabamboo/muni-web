9a694b2bbec455d94af55d9b7fa4c595b3a2a521
hamzaabamboo <hamzaabamboo@gmail.com>
2021-03-29 11:18:06 +0000

    Deploying to gh-pages from  @ d4bf5143bc1f126bc60e70dab88be1a8c95a85cd 🚀

--- _next/static/G7P1e8r1p5ynPody-j8O6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-3436171bf2d5fadcfd1b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f40cd832569abf61c05b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
