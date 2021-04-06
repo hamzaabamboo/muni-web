94df8f80d92b97167a55a0613e9ffaf477cb59e2
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-04-06 18:22:19 +0700

    Towa forgot event number

--- pages/event/[id].tsx
+++ pages/event/[id].tsx
@@ -144,6 +144,7 @@ export async function getStaticProps({ params }) {
     props: {
       event,
       points,
+      title: `Create むに web | ${event.name}`,
       isEventPage: true,
     },
   };
