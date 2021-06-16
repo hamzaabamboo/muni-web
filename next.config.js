aebfb613461d1a963d66c9e397ffa5405a1e1e71
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-06-16 21:57:05 +0700

    Towa upgrade nextjs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
   basePath: ghPages ? "/muni-web" : process.env.NEXT_PUBLIC_BASE_URL || "",
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   images: {
     domains: ["projectdivar.com"],
   },
