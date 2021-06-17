632bf5f0d7fc753d673580d96cb253a6a54d8dc7
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-06-17 14:46:12 +0700

    Towa update dependency and it's still doesn't work

--- next.config.js
+++ next.config.js
@@ -1,29 +1,18 @@
+const withTM = require("next-transpile-modules")(["d3-time-format", "d3-time"]);
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
-module.exports = {
+module.exports = withTM({
   basePath: ghPages ? "/muni-web" : process.env.NEXT_PUBLIC_BASE_URL || "",
-  webpack5: true,
   images: {
     domains: ["projectdivar.com"],
   },
   webpack: (config, { isServer, dev, webpack }) => {
-    config.output.chunkFilename = isServer
-      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
-      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
     config.plugins.push(
       new webpack.ProvidePlugin({
         PIXI: "pixi.js",
         spine: "pixi-spine",
       })
     );
-
-    config.output.hotUpdateMainFilename =
-      "static/webpack/[fullhash].[runtime].hot-update.json";
-
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-
     return config;
   },
-};
+});
