dee6e28c202d9098c9e4968d283491878295edd8
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2025-03-14 17:41:58 +0900

    fix: build error

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -12,9 +12,9 @@ export const Announcements = () => {
     <Alert status="info" my={2}>
       <AlertTitle mr={2}>Announcement</AlertTitle>
       <AlertDescription>
-        (2025/03/14) Hi, I'm back from the dead. we just hit over 5 million data
-        points the other day, crazy. So I updated data for past events for both
-        JP and EN, check it out&nbsp;
+        (2025/03/14) Hi, I&apos;m back from the dead. we just hit over 5 million
+        data points the other day, crazy. So I updated data for past events for
+        both JP and EN, check it out&nbsp;
         <Text as="span" colorScheme="blue">
           <Link href={"/event"}>here</Link>
         </Text>
